8e34bbed51c91c3e5617129ace41bd6384af6884
Andrés Ruiz Peñuela <andresruizpenuela@gmail.com>
2025-08-24 19:58:47 +0200

    [sts6] se crea el project y se realiza la configuración inicial

---
+++ sts6-restful-crud/src/main/java/com/codearp/application/Sts6RestfulCrudApplication.java
@@ -0,0 +1,13 @@
+package com.codearp.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sts6RestfulCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sts6RestfulCrudApplication.class, args);
+    }
+
+}
